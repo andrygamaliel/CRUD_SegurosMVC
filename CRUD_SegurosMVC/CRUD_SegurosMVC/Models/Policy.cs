7e344699c8d03dc40c6f63b3c97cad6d0da6289e
andry gamaliel <andrygonzalez077@gmail.com>
2025-02-08 23:01:36 -0400

    clase policy
    creación de la clase policy con sus propiedades

---
+++ CRUD_SegurosMVC/CRUD_SegurosMVC/Models/Policy.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_SegurosMVC.Models
+{
+    public class Policy
+    {
+        public int Id { get; set; }
+        public string PolicyNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal PremiumAmount { get; set; }
+    }
+}
