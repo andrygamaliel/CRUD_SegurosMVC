feec9cc01a45bbe1a1aeb94b1c80ed110fd4a875
andry gamaliel <andrygonzalez077@gmail.com>
2025-02-08 22:37:48 -0400

    creación del archivo raíz
    creación del archivo principal llamado: CRUD_SegurosMVC

---
+++ CRUD_SegurosMVC/CRUD_SegurosMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_SegurosMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
